89a82b1199d6f07771a59d8f8bf52275f56df92c
ramunsk <ramunsk@users.noreply.github.com>
2017-02-01 19:53:13 +0200

    Initial setup

---
+++ all.specs.js
@@ -0,0 +1,8 @@
+
+describe('Application should have tests for', () =>{
+    it('subscribing for all topics');
+    it('reconnecting to broker');
+    it('writing to database');
+    it('new topic handling');
+});
+
