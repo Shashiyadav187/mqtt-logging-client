88421f91a3d8e638fde3d13be59aedfb0a3d2367
ramunsk <ramunsk@users.noreply.github.com>
2017-02-10 22:23:47 +0200

    Moved db to dedicated folder

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const mqtt = require('mqtt');
 const config = require('./config');
-const DB = require('./db');
+const DB = require('./db/db');
 const Client = require('./client');
 
 new Client(config, mqtt, new DB()).run();
